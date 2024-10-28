82acc9a1472fa567ae41946ad47559cee5d0100b
Ali Asghar Gill <stylinalivlogs@gmail.com>
2024-10-28 17:00:09 +0500

    store and action created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import { useDispatch } from 'react-redux'
+import { addToCart } from './redux/action'
+import { productData } from './assets/ProductsData'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const dispatch = useDispatch()
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button type="button" onClick={() => dispatch(addToCart(productData))} className='font-bold text-red-500 text-2xl border-2 border-red-500 px-4 py-2 rounded hover:bg-red-500 hover:text-white ' >Add to Cart</button>
     </>
   )
 }
---
+++ src/assets/ProductsData.ts
@@ -0,0 +1,13 @@
+export const productData = {
+  id: 1,
+  name: "shoes",
+  price: 100,
+  description: "nice shoes",
+  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  category: "shoes",
+  rating: { rate: 3.9, count: 120 },
+  stock: 100,
+  isFeatured: true,
+  isBestseller: true,
+  isSale: true,
+};
